6b629f027c349054ad85f60e3bfb426d50f75b2a
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-05-11 22:03:27 +0700

    Experiment with in-place replacement.
    * dylanMain.js: Load dylanWorker lazily.
    
    * dylanWorker.js: Set up a couple of in-place replacements.
    
    * ticino.plugin.json: Add dylanWorker to the bundle config.

--- dylanMain.js
+++ dylanMain.js
@@ -2,4 +2,9 @@
 'use strict';
 define(["require", "exports", './dylanDef', 'monaco'], function (require, exports, dylanDef, monaco) {
     monaco.Modes.registerMonarchDefinition('dylan', dylanDef.language);
+    monaco.Modes.loadInBackgroundWorker(require.toUrl('./dylanWorker.js')).then(function (workerPiece) {
+        // worker piece loaded OK
+    }, function (err) {
+        console.error(err);
+    });
 });
