80f25f891f57900a7cfbc6948ae3233e0347e0e4
Peter Hull <peterhull90@gmail.com>
2020-11-12 07:26:12 +0000

    more progress

--- src/dylanTaskProvider.ts
+++ src/dylanTaskProvider.ts
@@ -1,25 +1,40 @@
 import * as vscode from 'vscode'
+import * as fs from 'fs'
 import { GetChannel } from './extension'
+import { resolve } from 'path'
 // See https://github.com/microsoft/vscode-extension-samples/blob/master/task-provider-sample/src/rakeTaskProvider.ts
 export class DylanTaskProvider implements vscode.TaskProvider {
     static Type = 'dylan-compiler'
+    _workspaceRoot: string | undefined
+    constructor(ws: string) {
+        this._workspaceRoot = ws
+    }
+    /* This is to scan  for tasks */
     provideTasks(_token: vscode.CancellationToken): vscode.ProviderResult<vscode.Task[]> {
         GetChannel().appendLine("*burp*");
+        // TODO scan all possible lid files from the workspace root.
+        const project = "testproject"
+        if (this._workspaceRoot) {
+        const projects = scanForProjects(this._workspaceRoot);
+        projects.then(ps=>GetChannel().appendLine(JSON.stringify(ps)));
+        }
         const compileTask = new vscode.Task({
             type: DylanTaskProvider.Type,
-            project: "cheese"
+            project: project
         },
             vscode.TaskScope.Workspace,
-            "build",
-            "dylan-compiler",
-            new vscode.ShellExecution("dylan-compiler -build cheese")
-            );
-            compileTask.group = vscode.TaskGroup.Build;
+            project,
+            DylanTaskProvider.Type,
+            new vscode.ProcessExecution("dylan-compiler", ["-build", project]),
+            "dylan-compiler-problems"
+        );
+
+        compileTask.group = vscode.TaskGroup.Build;
         GetChannel().appendLine(`Providing some tasks: ${JSON.stringify(compileTask)}`);
         return [compileTask];
     }
     resolveTask(task: vscode.Task, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Task> {
-        GetChannel().appendLine("Resolving some tasks");
+        GetChannel().appendLine(`Resolving some tasks:${JSON.stringify(task)}`);
         return task;
     }
 
@@ -27,3 +42,12 @@ export class DylanTaskProvider implements vscode.TaskProvider {
 interface DylanTaskDefinition extends vscode.TaskDefinition {
     project: string; // the project name
 }
+
+async function scanForProjects(path: string): Promise<string[]> {
+    return new Promise<string[]>((resolve, _reject) => {
+        fs.readdir(path, (err, files) => {
+            const projectFiles = files.filter(s => s.endsWith(".lid"));
+            resolve(projectFiles);
+        })
+    })
+} 
