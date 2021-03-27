7bf702094c1f5b565637441dd713118cb00af8f8
Peter Hull <peterhull90@gmail.com>
2021-03-27 16:14:03 +0000

    Get up-to-date with changes to server. Add code to find the server exe or specify its location manually

--- src/dylanLsp.ts
+++ src/dylanLsp.ts
@@ -5,7 +5,8 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { ExtensionContext } from 'vscode';
+import {existsSync} from 'fs';
+import  * as vscode from 'vscode';
 import {get_channel, get_compiler} from './extension';
 import {
 	LanguageClient,
@@ -23,13 +24,45 @@ function user_registries() {
 	}
 	return registries;
 }
+/**
+ * Find the Dylan LSP executable.
+ * Look on the PATH for `lsp-dylan`. 
+ * Can be overridden with `dylan.lsp` in the Settings.
+ * Returns undefined if nothing found.
+ */
+ function find_language_server(): string | undefined {
+    const config = vscode.workspace.getConfiguration('dylan')
+    const dylan_compiler = config.get<string>('lsp');
 
-export function activateLsp(context: ExtensionContext): void {
+    if (dylan_compiler) {
+		if (existsSync(dylan_compiler)){
+		get_channel().appendLine("LSP given explicitly "+dylan_compiler);
+        return dylan_compiler;
+		} else {
+			get_channel().appendLine("LSP given explicitly "+dylan_compiler+" but not found");
+			return undefined;
+		}
+    } else {
+        /* No setting, assume it's on the path */
+        const paths = (process.env.PATH || "").split(path.delimiter)
+        const exe = process.platform == "win32" ? "lsp-dylan.exe" : "lsp-dylan";
+        for (const p of paths) {
+            const dc = path.join(p, exe);
+            if (existsSync(dc)) {
+				get_channel().appendLine("LSP found on path "+dc);
+                return dc;
+            }
+        }
+		get_channel().appendLine("LSP not in "+paths+" or given explicitly");
+        return undefined; // Not found anywhere
+    }
+
+}
+export function activateLsp(context: vscode.ExtensionContext): void {
 	// The server is implemented in dylan native code
 	// TODO find the server from config, or from being on the path
-	const serverExe = context.asAbsolutePath(
-		path.join('..', '_build', 'bin', 'lsp-dylan')
-	);
+	const serverExe = find_language_server();
+	if (serverExe) {
 		get_channel().appendLine(`serverExe: ${serverExe}`)
 		get_channel().appendLine(`compiler: ${get_compiler()}`)
 	const openDylanRelease =
@@ -47,7 +80,7 @@ export function activateLsp(context: ExtensionContext): void {
 			options: runOptions
 		},
 		debug: {
-			command: serverExe, args: ['--debug'],
+			command: serverExe, args: ['--debug-server'],
 			options: runOptions
 		}
 	};
@@ -70,6 +103,11 @@ export function activateLsp(context: ExtensionContext): void {
 	// Start the client. This will also launch the server
 	const disposable = client.start();
 	context.subscriptions.push(disposable);
+	} else {
+		// Server not found.
+		vscode.window.showErrorMessage("Dylan language server not found. Advanced editing functions will not be available.")
+
+	}
 }
 
 export function deactivateLsp(): Thenable<void> | undefined {
