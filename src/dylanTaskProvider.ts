8b43cf433b456cff450e3f9547e1b0d0a041e34f
Peter Hull <peterhull90@gmail.com>
2021-06-05 19:52:31 +0100

    Formatting and other changes

--- src/dylanTaskProvider.ts
+++ src/dylanTaskProvider.ts
@@ -1,36 +1,36 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
-import { get_compiler } from './extension';
+import { getCompiler } from './extension'
 
 // See https://github.com/microsoft/vscode-extension-samples/blob/master/task-provider-sample/src/rakeTaskProvider.ts
 export class DylanTaskProvider implements vscode.TaskProvider {
-    static Type = 'dylan-compiler'
+  static Type = 'dylan-compiler'
 
-    /* This is to scan the workspace root for tasks */
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    provideTasks(_token: vscode.CancellationToken): Thenable<vscode.Task[]> {
-        return scanWorkspacesForTasks();
-    }
-    /* This is to "re-animate" a task that was stored in tasks.json */
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    resolveTask(task: vscode.Task, _token: vscode.CancellationToken): vscode.Task {
-        const dylanTaskDef = task.definition as DylanTaskDefinition;
-        const project = dylanTaskDef.project;
-        return new vscode.Task(task.definition,
-            vscode.TaskScope.Workspace,
-            project,
-            DylanTaskProvider.Type,
-            new vscode.ProcessExecution(get_compiler(), ["-build", project]),
-            task.problemMatchers
-        );
-    }
+  /* This is to scan the workspace root for tasks */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  provideTasks (_token: vscode.CancellationToken): Thenable<vscode.Task[]> {
+    return scanWorkspacesForTasks()
+  }
 
+  /* This is to "re-animate" a task that was stored in tasks.json */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  resolveTask (task: vscode.Task, _token: vscode.CancellationToken): vscode.Task {
+    const dylanTaskDef = task.definition as DylanTaskDefinition
+    const project = dylanTaskDef.project
+    return new vscode.Task(task.definition,
+      vscode.TaskScope.Workspace,
+      project,
+      DylanTaskProvider.Type,
+      new vscode.ProcessExecution(getCompiler(), ['-build', project]),
+      task.problemMatchers
+    )
+  }
 }
 /**
  * Extra info we need to associate with a Dylan compiler Task.
 */
 interface DylanTaskDefinition extends vscode.TaskDefinition {
-    project: string; // the project file name
+  project: string // the project file name
 }
 /**
  * Take a reference to a project file and generate the dylan-compiler
@@ -38,28 +38,28 @@ interface DylanTaskDefinition extends vscode.TaskDefinition {
  * @param p the URI of a project (.lid) file
  * @returns the Task
  */
-function projectUriToTask(p: vscode.Uri): vscode.Task {
-    const project = path.basename(p.fsPath);
-    const compileTask = new vscode.Task({
-        type: DylanTaskProvider.Type,
-        project: project
-    },
-        vscode.TaskScope.Workspace,
-        project,
-        DylanTaskProvider.Type,
-        new vscode.ProcessExecution(get_compiler(), ["-build", project]),
-        "$dylan-compiler-problems"
-    );
-    // TODO - set the working directory?
-    compileTask.group = vscode.TaskGroup.Build;
-    return compileTask;
+function projectUriToTask (p: vscode.Uri): vscode.Task {
+  const project = path.basename(p.fsPath)
+  const compileTask = new vscode.Task({
+    type: DylanTaskProvider.Type,
+    project: project
+  },
+  vscode.TaskScope.Workspace,
+  project,
+  DylanTaskProvider.Type,
+  new vscode.ProcessExecution(getCompiler(), ['-build', project]),
+  '$dylan-compiler-problems'
+  )
+  // TODO - set the working directory?
+  compileTask.group = vscode.TaskGroup.Build
+  return compileTask
 }
 /**
  * Look in all workspace folders for project files. Return a corresponding
  * array of configured Tasks.
  * @returns Array of tasks
  */
-async function scanWorkspacesForTasks(): Promise<vscode.Task[]> {
-    return vscode.workspace.findFiles("*.{lid,hdp}", "_build")
-        .then(uris => uris.map(projectUriToTask));
+async function scanWorkspacesForTasks (): Promise<vscode.Task[]> {
+  return await vscode.workspace.findFiles('*.{lid,hdp}', '_build')
+    .then(uris => uris.map(projectUriToTask))
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,34 @@
-import { existsSync } from 'fs';
-import * as vscode from 'vscode';
+import { existsSync } from 'fs'
+import * as vscode from 'vscode'
 import * as path from 'path'
-import { DylanTaskProvider } from './dylanTaskProvider';
+import { DylanTaskProvider } from './dylanTaskProvider'
 import { activateLsp, deactivateLsp } from './dylanLsp'
-let dylanTaskProvider: vscode.Disposable | undefined;
+let dylanTaskProvider: vscode.Disposable | undefined
 
-let channel: vscode.OutputChannel | undefined;
-export function get_channel(): vscode.OutputChannel {
-    if (!channel) {
-        channel = vscode.window.createOutputChannel('Dylan');
-    }
-    return channel;
+let channel: vscode.OutputChannel | undefined
+export function getChannel (): vscode.OutputChannel {
+  if (channel == null) {
+    channel = vscode.window.createOutputChannel('Dylan')
+  }
+  return channel
 }
 
-export function activate(context: vscode.ExtensionContext): void {
-    get_channel().appendLine('Activating');
-    compiler = find_compiler() || 'dylan-compiler-not-found';
-    activateLsp(context);
-    dylanTaskProvider = vscode.tasks.registerTaskProvider(DylanTaskProvider.Type, new DylanTaskProvider());
+export function activate (context: vscode.ExtensionContext): void {
+  getChannel().appendLine('Activating')
+  compiler = findCompiler() ?? 'dylan-compiler-not-found'
+  activateLsp(context)
+  dylanTaskProvider = vscode.tasks.registerTaskProvider(DylanTaskProvider.Type, new DylanTaskProvider())
 }
 
-export function deactivate(): Thenable<void> | undefined {
-    if (dylanTaskProvider) {
-        dylanTaskProvider.dispose();
-        dylanTaskProvider = void 0;
-    }
-    return deactivateLsp();
+export function deactivate (): Thenable<void> | undefined {
+  if (dylanTaskProvider != null) {
+    dylanTaskProvider.dispose()
+    dylanTaskProvider = undefined
+  }
+  return deactivateLsp()
 }
 // Cached location.
-let compiler: string | undefined = undefined;
+let compiler: string | undefined
 /**
  * Find the Dylan compiler.
  * Look on the PATH for `dylan-compiler` and `dylan-compiler-with-tools.exe`
@@ -36,29 +36,28 @@ let compiler: string | undefined = undefined;
  * Can be overridden with `dylan.compiler` in the Settings.
  * Returns undefined if nothing found.
  */
-function find_compiler(): string | undefined {
-    const config = vscode.workspace.getConfiguration('dylan')
-    const dylan_compiler = config.get('compiler');
-
-    if (dylan_compiler) {
-        return dylan_compiler as string;
-    } else {
-        /* No setting, assume it's on the path */
-        const paths = (process.env.PATH || "").split(path.delimiter)
-        const exe = process.platform == "win32" ? "dylan-compiler-with-tools.exe" : "dylan-compiler";
-        for (const p of paths) {
-            const dc = path.join(p, exe);
-            if (existsSync(dc)) {
-                return dc;
-            }
-        }
-        return undefined; // Not found anywhere
+function findCompiler (): string | undefined {
+  const config = vscode.workspace.getConfiguration('dylan')
+  const dylanCompiler = config.get('compiler')
+  if (dylanCompiler != null && dylanCompiler !== '') {
+    return dylanCompiler as string
+  } else {
+    /* No setting, assume it's on the path */
+    const paths = (process.env.PATH ?? '').split(path.delimiter)
+    const exe = process.platform === 'win32' ? 'dylan-compiler-with-tools.exe' : 'dylan-compiler'
+    for (const p of paths) {
+      const dc = path.join(p, exe)
+      if (existsSync(dc)) {
+        return dc
+      }
     }
-
+    return undefined // Not found anywhere
+  }
 }
 /**
- * Get full path of the dylan compiler
+ * Get full path of the dylan compiler.
+ * This is set when the extension activates and returned from the cache.
  */
-export function get_compiler(): string {
-    return compiler as string;
+export function getCompiler (): string {
+  return compiler as string
 }
